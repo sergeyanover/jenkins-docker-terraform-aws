28014a68ffe464a0c4ed6ccfcec39de5bb7e2a6c
Sergey Yanover <sergey.yanover@gmail.com>
2021-07-01 23:44:54 +0300

    01.07.2021

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -13,4 +13,4 @@ cd jenkins
 docker build -t jenkins .
 docker tag jenkins aws-docker-jenkins:v1
 
-docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home aws-docker-jenkins:v1
+docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home aws-docker-jenkins:v1

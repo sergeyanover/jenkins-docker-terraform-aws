da48dbc67352fd438f2ba84faeb2cf6ff4021ed0
Sergey Yanover <sergey.yanover@gmail.com>
2021-07-01 23:19:05 +0300

    01.07.2021

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -7,10 +7,10 @@ sudo usermod -aG docker ec2-user
 docker pull alpine/git
 
 
-docker run -ti --rm -v /home/ec2-user:/root -v /home/ec2-user:/git alpine/git clone https://github.com/sergeyanover/jenkins-docker-terraform-aws.git
+sudo docker run -ti --rm -v /home/ec2-user:/root -v /home/ec2-user:/git alpine/git clone https://github.com/sergeyanover/jenkins-docker-terraform-aws.git
 cd jenkins-docker-terraform-aws
 cd jenkins
-docker build -t jenkins .
-docker tag jenkins aws-docker-jenkins:v1
+sudo docker build -t jenkins .
+sudo docker tag jenkins aws-docker-jenkins:v1
 
-docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home aws-docker-jenkins:v1
+sudo docker run --name jenkins -d -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home aws-docker-jenkins:v1
